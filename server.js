e2e0ee75adad01a61ac7672d1f996aad0011fea5
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2019-07-01 07:07:13 +0200

    Refactor: move caching functionality to seperate module (SRP)

--- server.js
+++ server.js
@@ -1,56 +1,10 @@
 const express = require('express');
 const next = require('next');
-const LRUCache = require('lru-cache');
+const getFromCacheOrCreate = require('./server/responseCache');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-
-const cache = new LRUCache({
-    // cache size will be 100 MB using `return n.length` as length() function
-    max: 100 * 1024 * 1024,
-    length: function(n) {
-        return n.length;
-    },
-    maxAge: 1000 * 60 * 60 * 24, // 24 hours
-});
-
-const createCacheKey = request => request.path;
-
-const getFromCache = request => cache.get(createCacheKey(request));
-
-const writeToCache = (request, response) =>
-    cache.set(createCacheKey(request), response);
-
-const getFromCacheOrCreate = async (request, response, createResponseBody) => {
-    console.debug(' --> fetch', request.path);
-
-    const existingResponseBody = getFromCache(request);
-
-    if (existingResponseBody) {
-        console.debug(' --> existing cache', request.path);
-
-        return [existingResponseBody, true];
-    }
-
-    console.debug(' --> create new cache', request.path);
-
-    const createdResponseBody = await createResponseBody(request, response);
-
-    // only cache response when it is a successful request
-    if (response.statusCode === 200) {
-        console.debug(' --> write to cache', request.path);
-
-        writeToCache(request, createdResponseBody);
-    } else {
-        console.warn(
-            ` --> don't write to cache due to ${response.statusCode} response`,
-            request.path
-        );
-    }
-
-    return [createdResponseBody, false];
-};
 
 const createResponseBody = async (request, response) => {
     return await app.renderToHTML(
